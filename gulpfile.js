dcb111db71f74264ebb0fe2b7d6116449407bc6a
Zachary Todd <github@zttodd.com>
2019-02-16 20:56:06 -0500

    Add html templates to gulp; clean up tasks
    - Added internal fs, gulp-template, gulp-data, and data folder for HTML
    templating through Gulp.
    - Added html task.
    - Deleted old "css" folder and root index.html
    - Added file extensions to gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 'use strict';
 
 const { src, dest, parallel, watch } = require('gulp');
+const data = require('gulp-data');
+const fs = require('fs');
 const uglify = require('gulp-uglify');
 const rimraf = require('rimraf');
 const sass = require('gulp-sass');
+const template = require('gulp-template');
 
 function clean(done) {
     rimraf('./build', function() {});
     rimraf('./dist', function() {});
     done()
+}
+
+function html() {
+    return src('src/**/*.html')
+        .pipe(data(function() {
+            return JSON.parse(fs.readFileSync('./data/site.json'));
+        }))
+        .pipe(template())
+        .pipe(dest('./build'))
+        .pipe(dest('./dist'))
 }
 
 // Copy images from source files into 'build' and 'dist'
@@ -19,7 +32,7 @@ function images() {
 }
 
 function scripts() {
-    return src('./src/js/**/*')
+    return src('./src/js/**/*.js')
         .pipe(dest('./build/js'))
         .pipe(uglify())
         .pipe(dest('./dist/js'))
@@ -37,10 +50,11 @@ function styles() {
 }
 
 function watchFiles() {
-    watch("./src/js/**/*", scripts);
-    watch("./src/scss/**/*", styles);
+    watch("./src/**/*.html", html);
+    watch("./src/js/**/*.js", scripts);
+    watch("./src/scss/**/*.scss", styles);
 }
 
 exports.clean = clean;
 exports.images = images;
-exports.default = parallel(styles, images, scripts, watchFiles);
+exports.default = parallel(html, images, scripts, styles, watchFiles);
