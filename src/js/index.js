eb99475cd6d6e45ce9fa2418f76041dc31d9f16f
Zachary Todd <github@zttodd.com>
2019-02-28 23:20:01 -0500

    Store light/dark mode in localStorage
    - Stores value for whether user has chosen light or dark mode and stores
    it in localStorage so that the mode will persist page reload.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,15 @@
-// create buttons
 const body = document.getElementsByTagName("body")[0];
 
+// set class based on current mode
+if (localStorage.getItem("mode") === "light") {
+  body.classList.remove("dark");
+  body.classList.add("light");
+} else if (localStorage.getItem("mode") === "dark") {
+  body.classList.remove("light");
+  body.classList.add("dark");
+}
+
+// create buttons
 const buttonDiv = document.createElement("div");
 buttonDiv.classList.add("modes");
 
@@ -34,10 +43,12 @@ for (let index = 0; index < buttonProps.length; index++) {
   button.addEventListener("click", function() {
     body.classList = "";
     body.classList.add(buttonProps[index].class);
+    localStorage.setItem("mode", buttonProps[index].class);
   });
 
   // append button to buttonDiv
   buttonDiv.appendChild(button);
 }
 
+// add button container to body
 body.appendChild(buttonDiv);
