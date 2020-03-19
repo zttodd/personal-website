6a619168b6107c8b736ffd7dea20353655ec5f67
Zachary Todd <github@zttodd.com>
2020-03-18 20:59:43 -0400

    Refactored Sass build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
-function sass(cb) {
-  // place code for your default task here
-  cb();
+const { src, dest, series, watch } = require('gulp');
+const sass = require('gulp-sass');
+
+// Set compiler to Dart Sass
+sass.compiler = require('sass');
+
+// Build Sass into CSS
+function compileSass() {
+  return src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./_site/css'));
 }
 
-exports.default = sass;
+// Watch Sass files for changes
+function watchSass() {
+  watch('./src/sass/**/*.scss', compileSass);
+}
+
+exports.default = series(compileSass, watchSass);
