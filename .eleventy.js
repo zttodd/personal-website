56eff580bd8c668cff860055c73630ed2436b87f
zttodd <github@zttodd.com>
2020-07-19 11:57:29 -0400

    Added readableDate filter to eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
-module.exports = {
-  dir: {
-    input: ".",
-    includes: "_includes",
-    data: "_data",
-    output: "_site"
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("LLL dd, yyyy");
+  });
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
   }
 };
