67bb92ed38f260fcb75d54f25f29ffdc7451b4f4
Zachary Todd <github@zttodd.com>
2020-07-19 14:34:03 -0400

    Added htmlDateString filter to eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,11 @@ module.exports = function (eleventyConfig) {
     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("LLL dd, yyyy");
   });
 
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
+
   return {
     dir: {
       input: ".",
@@ -12,5 +17,5 @@ module.exports = function (eleventyConfig) {
       data: "_data",
       output: "_site"
     }
-  }
+  };
 };
